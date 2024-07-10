3a8fc6c098f91fa1953a4e684bd5fdff87b4fcce
bloopgoop <zhu.kevin12@gmail.com>
2024-07-10 18:23:08 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,8 +9,18 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # wait for client
+    conn, addr = server_socket.accept()
+    data = conn.recv(1024)
+    req = data.split(b"\r\n")
+    req_line = req[0]
+    route = req_line.split()[1]
 
+    if route == b"/":
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
