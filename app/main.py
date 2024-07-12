cd3a972ea01b9d4c64a373a5a68c277ba61477a4
bloopgoop <zhu.kevin12@gmail.com>
2024-07-12 17:20:11 -0400

    concurrent connections attempt 1

--- app/main.py
+++ app/main.py
@@ -1,18 +1,11 @@
 # Uncomment this to pass the first stage
 import socket
 import re
+import threading
 
-OK_RESPONSE = b"HTTP/1.1 200 OK\r\n\r\n"
-NOTFOUND_RESPONSE = b"HTTP/1.1 404 Not Found\r\n\r\n"
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, _ = server_socket.accept()
+def request_handler(conn: socket.socket):
+    """ Function that handles client request """
     request = conn.recv(1024).decode() # decode bytes to string
     url = re.search("GET (.*) HTTP", request).group(1) # get first match of this regex search to get the req url
 
@@ -29,6 +22,19 @@ def main():
         conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
     conn.close()
+
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        conn, _ = server_socket.accept()
+        thread = threading.Thread(target=request_handler, args=(conn,))
+        thread.start()
+
 
 if __name__ == "__main__":
     main()
